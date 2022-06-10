2becf97d012191c3715f7b3911a0fd72a3e43af7
Sofia Krylova <sofia_krylova@epam.com>
2022-06-10 15:38:29 +0200

    Initial

---
+++ src/main/java/com/epam/autotasks/ShelterService.java
@@ -0,0 +1,18 @@
+package com.epam.autotasks;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class ShelterService {
+
+    public void assignAttendants(List<ShelterRoom> rooms) {
+    }
+
+    public List<Cat> getCheckUpList(List<ShelterRoom> rooms, LocalDate date) {
+        return null;
+    }
+
+    public List<Cat> getCatsByBreed(List<ShelterRoom> rooms, Cat.Breed breed) {
+        return null;
+    }
+}
